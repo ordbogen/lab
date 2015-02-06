aa97dcdaf920f0b5fce84c36849ca6b25348613f
Thomas B Homburg <thomas@homburg.dk>
2015-02-06 20:50:36 +0100

    Remove stub cli actions

--- main.go
+++ main.go
@@ -486,25 +486,6 @@ func main() {
 						}
 					},
 				},
-				{
-					Name:  "create",
-					Usage: "create a merge request",
-					Action: func(c *cli.Context) {
-						log.Fatal(`Create merge request:
-1. lab mr create -> <current-branch>..<lab default branch>
-2. lab mr create <target branch> ->  <current-branch>..<target branch>
-3. lab mr create <source branch>..<target branch> -> <source branch>..<target branch>
-					`)
-					},
-				},
-				{
-					// Accept using the current branch or a given mr id
-					Name:  "accept",
-					Usage: "accept merge request by the current branch",
-					Action: func(c *cli.Context) {
-						log.Fatal("TODO: lab mr accept [<id>]")
-					},
-				},
 			},
 		},
 	}
