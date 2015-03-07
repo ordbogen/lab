525f325bcc501b7d92be5f34e0d55e11364eaeae
Thomas B Homburg <thomas@homburg.dk>
2015-03-08 00:37:37 +0100

    Add tests and use goconvey

--- gitlab_test.go
+++ gitlab_test.go
@@ -10,52 +10,180 @@ import (
 	"testing"
 )
 
-func TestGetSession(t *testing.T) {
-	var req sessionRequest
-	var method string
-	var requestUrl string
-
-	sr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func serveAndCatchJson(t *testing.T, jsonOut interface{}) (*httptest.Server, chan *http.Request) {
+	reqChan := make(chan *http.Request, 1)
+	serv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqChan <- r
 		dec := json.NewDecoder(r.Body)
-		err := dec.Decode(&req)
+		err := dec.Decode(jsonOut)
 		if nil != err {
 			t.Fatal(err)
 		}
-
-		requestUrl = r.URL.String()
-		method = r.Method
 	}))
+	return serv, reqChan
+}
 
-	u, err := url.Parse(sr.URL)
+func urlMustParse(t *testing.T, urlStr string) *url.URL {
+	u, err := url.Parse(urlStr)
 	if nil != err {
 		t.Fatal(err)
 	}
+	return u
+}
 
-	g := newGitlab(u.Host)
-	g.getSession("user", "password")
+func TestGetSession(t *testing.T) {
+	Convey("Given a gitlab server", t, func() {
+		var reqData sessionRequest
 
-	expectedUrl := fmt.Sprintf("http://%s/api/v3/session", u.Host)
-	if "POST" != method || expectedUrl != requestUrl {
-		t.Fatalf("Expected POST request to %q, got %s request to %q", expectedUrl, method, requestUrl)
-	}
+		sr, reqChan := serveAndCatchJson(t, &reqData)
+		u := urlMustParse(t, sr.URL)
 
-	expectedUser := "user"
-	if expectedUser != req.Login {
-		t.Fatalf("Expected login: %q, got %q\n", expectedUser, req.Login)
-	}
+		g := newGitlab(u.Host)
 
-	expectedPassword := "password"
-	if expectedUser != req.Login {
-		t.Fatalf("Expected password: %q, got %q\n", expectedPassword, req.Password)
-	}
+		Convey("When requesting a session (private token)", func() {
+			g.getSession("user", "password")
+
+			Convey("The client should post the correct json to the api", func() {
+				expectedUrl := fmt.Sprintf("http://%s/api/v3/session", u.Host)
+				req := <-reqChan
+				So(req.Method, ShouldEqual, "POST")
+				So(req.URL.String(), ShouldEqual, expectedUrl)
+
+				So(reqData, ShouldResemble, sessionRequest{"user", "password"})
+			})
+		})
+	})
 }
 
 func testGetPrivateTokenUrl(t *testing.T) {
 	Convey("Given a gitlab instance", t, func() {
 		g := newGitlab("1.2.3.4")
 
-		Convey("It should produce a url for obtaining a private token", t, func() {
+		Convey("It should produce a url for obtaining a private token", func() {
+			So("something", ShouldEqual, "something else")
 			So(g.getPrivateTokenUrl(), ShouldEqual, "http://1.2.3.4/profile/account")
+		})
+	})
+}
+
+func TestCreateMergeRequest(t *testing.T) {
+	Convey("Given a merge request", t, func() {
+		var mr mergeRequestCreateRequest
+
+		sr, reqChan := serveAndCatchJson(t, &mr)
+		u := urlMustParse(t, sr.URL)
+		g := newGitlab(u.Host)
+		g.token = "my-private-token"
+
+		Convey("When creating a merge request", func() {
+			g.createMergeRequest("17", "source-branch", "target-branch", "my title")
+
+			Convey("The request should match", func() {
+				req := <-reqChan
+				So(req.Method, ShouldEqual, "POST")
+				So(
+					req.URL.String(),
+					ShouldEqual,
+					fmt.Sprintf("http://%s/api/v3/projects/17/merge_requests?private_token=my-private-token", u.Host),
+				)
+				So(
+					mr,
+					ShouldResemble,
+					mergeRequestCreateRequest{
+						Title:        "my title",
+						SourceBranch: "source-branch",
+						TargetBranch: "target-branch",
+					},
+				)
+			})
+		})
+	})
+}
+
+func TestQueryMergeRequests(t *testing.T) {
+	Convey("Given a gitlab server", t, func() {
+		var req *http.Request
+		mrs := []mergeRequest{
+			mergeRequest{
+				Id:           13,
+				Iid:          17,
+				Title:        "my-title",
+				Description:  "my-description",
+				SourceBranch: "source-branch",
+				TargetBranch: "target-branch",
+			},
+		}
+
+		sr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			enc := json.NewEncoder(w)
+			err := enc.Encode(mrs)
+			if nil != err {
+				t.Fatal(err)
+			}
+			req = r
+		}))
+
+		u := urlMustParse(t, sr.URL)
+		g := newGitlab(u.Host)
+		g.token = "my-private-token"
+
+		Convey("When creating a merge request", func() {
+			gottenMrs, err := g.queryMergeRequests("17", "shuffled")
+
+			Convey("The request should match", func() {
+				So(err, ShouldBeNil)
+				So(gottenMrs, ShouldResemble, mrs)
+				So(req.Method, ShouldEqual, "GET")
+				So(
+					req.URL.String(),
+					ShouldEqual,
+					fmt.Sprintf(
+						"http://%s/api/v3/projects/17/merge_requests?private_token=my-private-token&state=shuffled",
+						u.Host,
+					),
+				)
+			})
+		})
+	})
+}
+
+func TestQueryMergeRequestsErrorMessage(t *testing.T) {
+	Convey("Given a gitlab server", t, func() {
+		var req *http.Request
+
+		sr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(417)
+			enc := json.NewEncoder(w)
+			err := enc.Encode(map[string]string{
+				"message": "my error message",
+			})
+			if nil != err {
+				t.Fatal(err)
+			}
+			req = r
+		}))
+
+		u := urlMustParse(t, sr.URL)
+		g := newGitlab(u.Host)
+		g.token = "my-private-token"
+
+		Convey("When creating a merge request", func() {
+			_, err := g.queryMergeRequests("17", "shuffled")
+
+			Convey("The request should match", func() {
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldContainSubstring, "my error message")
+				So(req.Method, ShouldEqual, "GET")
+				So(
+					req.URL.String(),
+					ShouldEqual,
+					fmt.Sprintf(
+						"http://%s/api/v3/projects/17/merge_requests?private_token=my-private-token&state=shuffled",
+						u.Host,
+					),
+				)
+			})
 		})
 	})
 }
